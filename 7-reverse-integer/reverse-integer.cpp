778b3cca48ec092dd0efad4923173f0f208a7bc5
arvakhatore2006-cyber <arvakhatore2006@gmail.com>
2026-06-25 23:27:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (98.18%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int revNum = 0;
+
+        while (x != 0) {
+            int lastdigit = x % 10;
+            
+            // Check overflow
+            if (revNum > INT_MAX / 10 || revNum < INT_MIN / 10)
+                return 0;
+
+            revNum = (revNum * 10) + lastdigit;
+            x = x / 10;
+        }
+
+        return revNum;
+    }
+};
