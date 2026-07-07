83b8fc07ba3aff6e6f4dcba82d7735db07c6fcee
Arva Khatore <arvakhatore2006@gmail.com>
2026-07-07 23:41:18 +0530

    Time: 10 ms (71.43%) | Memory: 35.2 MB (19.46%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    // Function to calculate the number of days required
+    int findDays(vector<int>& weights, int cap) {
+        int days = 1;
+        int load = 0;
+
+        for (int i = 0; i < weights.size(); i++) {
+            if (load + weights[i] > cap) {
+                days++;
+                load = weights[i];
+            } else {
+                load += weights[i];
+            }
+        }
+
+        return days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int numberOfDays = findDays(weights, mid);
+
+            if (numberOfDays <= days) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
