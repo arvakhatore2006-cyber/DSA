b78a766735a5f625ff11cd8ac611f47ea9ad6a33
Arva Khatore <arvakhatore2006@gmail.com>
2026-09-03 16:00:35 +0530

    Time: 974 ms (82.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+   Employee.name,
+   Bonus.bonus
+FROM
+   Employee
+LEFT JOIN
+   Bonus
+ON
+   Employee.empId = Bonus.empId
+WHERE
+   IFNULL(Bonus.bonus, 0) < 1000;
